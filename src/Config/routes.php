3e882c0b82ae7b7d6ac6c2ad81a935e3390fc9ac
Robert Brown <brownrl@gmail.com>
2025-08-25 09:52:37 +0200

    moving to ec-doris

--- src/Config/routes.php
+++ src/Config/routes.php
@@ -4,7 +4,7 @@
  * For the full copyright and license information, please view
  * the LICENSE file that was distributed with this source code.
  *
- * @see https://github.com/brownrl/laravel-cas
+ * @see https://github.com/ec-doris/laravel-cas
  */
 
 declare(strict_types=1);
